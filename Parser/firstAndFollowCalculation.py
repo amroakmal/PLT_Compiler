c67f68d3e947523ea1e504943eace4c69ba91b6e
wadie bishoy <wadie.bishoy25@gmail.com>
2021-06-04 22:18:28 +0200

    Create the predictive table

--- Parser/firstAndFollowCalculation.py
+++ Parser/firstAndFollowCalculation.py
@@ -97,6 +97,91 @@ while True:
     if follow_pre == follow:
         break
 
-for x in range(len(follow)):
-    print(non_terminals[x], '----', follow[x])
+# --------------------------------------table----------------------------------
 
+terminals = set()
+
+for x in range(len(non_terminals)):
+    for f in first[x]:
+        terminals.add(f)
+    for f in follow[x]:
+        terminals.add(f)
+
+terminals.discard('\\L')
+terminals = list(terminals)
+
+pred_table = []
+t = [-1] * len(terminals)
+for x in range(len(non_terminals)):
+    pred_table.append(copy.deepcopy(t))
+
+
+
+mod_production = []
+
+for i in range(len(production)):
+    r = production[i].split("=", 1)[1]
+    list_prod = r.split("|")
+    for k in range(len(list_prod)):
+        candidate = list_prod[k].strip()
+        new_production = non_terminals[i] + " = " + candidate
+        mod_production.append(new_production)
+
+for x in range(len(non_terminals)):
+    print(non_terminals[x], " -- ", first[x])
+print()
+for x in range(len(non_terminals)):
+    print(non_terminals[x], " ## ", follow[x])
+print()
+for x in range(len(mod_production)):
+    print(x, " - ", mod_production[x])
+
+
+
+
+
+for i in range(len(mod_production)):
+    left = mod_production[i].split("=", 1)[0].strip()
+    right = mod_production[i].split("=", 1)[1].strip()
+    list_cand = right.split(" ")
+    c = 0
+    while True:
+        if list_cand[c] == '\L':
+            for fo in follow[non_terminals.index(left)]:
+                pred_table[non_terminals.index(left)][terminals.index(fo)] = i
+            break
+        elif list_cand[c] not in non_terminals:
+            pred_table[non_terminals.index(left)][terminals.index(list_cand[c])] = i
+            break
+        else:
+            for f in first[non_terminals.index(list_cand[c])]:
+                if f != '\L':
+                    pred_table[non_terminals.index(left)][terminals.index(f)] = i
+            if '\L' not in first[non_terminals.index(list_cand[c])]:
+                break
+            if c == len(list_cand) - 1:
+                for fo in follow[non_terminals.index(left)]:
+                    pred_table[non_terminals.index(left)][terminals.index(fo)] = i
+                break
+            c += 1
+
+
+
+
+
+
+
+def print_table(term, non_term, table):
+    print('\n', "{:<23}".format(""), end="", flush=True)
+    for i in term:
+        print("{:<10}".format(str(i)), end="", flush=True)
+    print()
+
+    for x in range(len(non_term)):
+        print("{:<22} {:<2}".format(non_term[x], '|'), end="", flush=True)
+        for i in table[x]:
+            print("{:<10}".format(str(i)), end="", flush=True)
+        print()
+
+
+print_table(terminals, non_terminals, pred_table)
